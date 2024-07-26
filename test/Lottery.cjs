44820017f050ea039380cda8048fff5c56d97b42
jimjimvalkema <jjvalkema@protonmail.com>
2024-07-26 19:05:53 +0200

    made test to demostrate reproducing the merkle tree from logs

--- test/Lottery.cjs
+++ test/Lottery.cjs
@@ -1,7 +1,8 @@
 const { expect } = require("chai");
 const hre = require("hardhat");
 const { proxy, PoseidonT3 } = require('poseidon-solidity')
-
+const {poseidon2} = require("poseidon-lite");
+const {MerkleTree} = require( "fixed-merkle-tree");
 
 const {
   time,
@@ -36,7 +37,6 @@ describe("Lottery", function () {
       return Poseidon.attach(deployInfo.address)
     }
     const _poseidon = await Poseidon.deploy()
-    console.log({_poseidon})
     return _poseidon
   }
 
@@ -58,129 +58,29 @@ describe("Lottery", function () {
 
   it("Should deploy", async function () {
     const { lottery } = await deploy()
-    console.log(await lottery.getLastRoot())
+    expect(await ethers.provider.getCode(lottery.target)).to.not.equal("0x")
+
+
+  })
+
+  it("Match offchain and onchain roots",  async function () {
+    const { lottery } = await deploy()
+
+    // buy a ticket
     await lottery.buyTicket(hre.ethers.zeroPadValue("0x01", 32))
-    console.log(await lottery.getLastRoot())
+
+    // get chain data
+    const purchaseEventFilter = lottery.filters.Purchase() 
+    const events = await lottery.queryFilter(purchaseEventFilter, 0,"latest")
+    const onchainLeaves = events.map((event)=>event.topics[1])
+    const merkleTreeDepth = await lottery.levels()
+
+    // reproduce tree
+    const hashFunction = (left, right)=>poseidon2([left, right])
+    const tree = new MerkleTree(Number(merkleTreeDepth), onchainLeaves, {hashFunction, zeroElement:hre.ethers.zeroPadValue("0x00", 32)})
+
+    expect(await lottery.getLastRoot()).to.equal(hre.ethers.toBeHex( tree.root));
+
 
   })
 })
-
-// describe("Lock", function () {
-//   // We define a fixture to reuse the same setup in every test.
-//   // We use loadFixture to run this setup once, snapshot that state,
-//   // and reset Hardhat Network to that snapshot in every test.
-//   async function deployOneYearLockFixture() {
-//     const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
-//     const ONE_GWEI = 1_000_000_000;
-
-//     const lockedAmount = ONE_GWEI;
-//     const unlockTime = (await time.latest()) + ONE_YEAR_IN_SECS;
-
-//     // Contracts are deployed using the first signer/account by default
-//     const [owner, otherAccount] = await ethers.getSigners();
-
-//     const Lock = await ethers.getContractFactory("Lock");
-//     const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
-
-//     return { lock, unlockTime, lockedAmount, owner, otherAccount };
-//   }
-
-//   describe("Deployment", function () {
-//     it("Should set the right unlockTime", async function () {
-//       const { lock, unlockTime } = await loadFixture(deployOneYearLockFixture);
-
-//       expect(await lock.unlockTime()).to.equal(unlockTime);
-//     });
-
-//     it("Should set the right owner", async function () {
-//       const { lock, owner } = await loadFixture(deployOneYearLockFixture);
-
-//       expect(await lock.owner()).to.equal(owner.address);
-//     });
-
-//     it("Should receive and store the funds to lock", async function () {
-//       const { lock, lockedAmount } = await loadFixture(
-//         deployOneYearLockFixture
-//       );
-
-//       expect(await ethers.provider.getBalance(lock.target)).to.equal(
-//         lockedAmount
-//       );
-//     });
-
-//     it("Should fail if the unlockTime is not in the future", async function () {
-//       // We don't use the fixture here because we want a different deployment
-//       const latestTime = await time.latest();
-//       const Lock = await ethers.getContractFactory("Lock");
-//       await expect(Lock.deploy(latestTime, { value: 1 })).to.be.revertedWith(
-//         "Unlock time should be in the future"
-//       );
-//     });
-//   });
-
-//   describe("Withdrawals", function () {
-//     describe("Validations", function () {
-//       it("Should revert with the right error if called too soon", async function () {
-//         const { lock } = await loadFixture(deployOneYearLockFixture);
-
-//         await expect(lock.withdraw()).to.be.revertedWith(
-//           "You can't withdraw yet"
-//         );
-//       });
-
-//       it("Should revert with the right error if called from another account", async function () {
-//         const { lock, unlockTime, otherAccount } = await loadFixture(
-//           deployOneYearLockFixture
-//         );
-
-//         // We can increase the time in Hardhat Network
-//         await time.increaseTo(unlockTime);
-
-//         // We use lock.connect() to send a transaction from another account
-//         await expect(lock.connect(otherAccount).withdraw()).to.be.revertedWith(
-//           "You aren't the owner"
-//         );
-//       });
-
-//       it("Shouldn't fail if the unlockTime has arrived and the owner calls it", async function () {
-//         const { lock, unlockTime } = await loadFixture(
-//           deployOneYearLockFixture
-//         );
-
-//         // Transactions are sent using the first signer by default
-//         await time.increaseTo(unlockTime);
-
-//         await expect(lock.withdraw()).not.to.be.reverted;
-//       });
-//     });
-
-//     describe("Events", function () {
-//       it("Should emit an event on withdrawals", async function () {
-//         const { lock, unlockTime, lockedAmount } = await loadFixture(
-//           deployOneYearLockFixture
-//         );
-
-//         await time.increaseTo(unlockTime);
-
-//         await expect(lock.withdraw())
-//           .to.emit(lock, "Withdrawal")
-//           .withArgs(lockedAmount, anyValue); // We accept any value as `when` arg
-//       });
-//     });
-
-//     describe("Transfers", function () {
-//       it("Should transfer the funds to the owner", async function () {
-//         const { lock, unlockTime, lockedAmount, owner } = await loadFixture(
-//           deployOneYearLockFixture
-//         );
-
-//         await time.increaseTo(unlockTime);
-
-//         await expect(lock.withdraw()).to.changeEtherBalances(
-//           [owner, lock],
-//           [lockedAmount, -lockedAmount]
-//         );
-//       });
-//     });
-//   });
-// });
